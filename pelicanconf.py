d8b1aba52c0350f66ad9962962c6b528b929f051
frezzz1 <skonovalov389@gmail.com>
2024-12-21 00:09:49 +0300

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+AUTHOR = 'Konovalov Sergei'
+SITENAME = 'My_CV'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'Russian'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
